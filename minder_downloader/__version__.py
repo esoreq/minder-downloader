28da9d7bc97664010c98a4b17b499ce221c057b3
Soreq <e.soreq14@imperial.ac.uk>
2023-04-18 09:44:37 +0300

    The config module now includes two functions that create a .minder folder at the home level. These functions first check if the environment variables MINDER_DOWNLOADER_HOME and RESEARCH_PORTAL_TOKEN_PATH exist. If they do, the functions use them as root folders and create a config folder there. Alternatively, if these environment variables are not set, the functions create the folder at the home directory. This will also spawn the interactive call to get a token.

--- minder_downloader/__version__.py
+++ minder_downloader/__version__.py
@@ -2,4 +2,4 @@
 __author__ = "Eyal Soreq"
 __copyright__ = "2023 Eyal Soreq"
 __title__ = "minder_downloader"
-__version__ = "0.1.06"
+__version__ = "0.1.07"
--- minder_downloader/config.py
+++ minder_downloader/config.py
@@ -18,23 +18,45 @@ def check_config():
     if CONFIG_CHECKED:
         return
     CONFIG_CHECKED = True
-    
-    TOKEN = os.environ.get('RESEARCH_PORTAL_TOKEN_PATH') # check if token is set at the environment level
-    root = os.environ.get('MINDER_DOWNLOADER_HOME', Path(__file__).parent)
+
+    root = check_root_folder()
+
+    check_token_file(root) # check if token is set at the environment level
+        
+
+
+def check_root_folder():
+    root = os.environ.get('MINDER_DOWNLOADER_HOME')
+    if root is None:
+        root = Path.home()
+    minder_home = Path(f'{root}{os.sep}.minder')
+    if not minder_home.exists():
+        minder_home.mkdir(parents=True)
+        os.environ['MINDER_DOWNLOADER_HOME'] = f'{minder_home}'
+    return f'{minder_home}'
+
+
+def check_token_file(root):
+    token_path = os.environ.get('RESEARCH_PORTAL_TOKEN_PATH') 
+    TOKEN = None 
     info_path = f'{root}{os.sep}info.yaml'
-    if not path_exists(info_path):
-        tmp = {'headers':{ 'Accept': 'text/plain',
-                        'Connection': 'keep-alive',
-                        'Content-type': 'application/json'},  
-                'server': 'https://research.minder.care/api'}
+    tmp = {'headers':{ 'Accept': 'text/plain',
+                    'Connection': 'keep-alive',
+                    'Content-type': 'application/json'},  
+            'server': 'https://research.minder.care/api'}
+    if token_path is None:
+        if path_exists(info_path):
+            tmp = load_yaml(info_path)
+        if 'token' in tmp.keys():    
+            TOKEN = tmp['token']
+        else:     
+            TOKEN = get_token()
+    elif Path(token_path).exists(): 
+        with open(token_path, 'r') as file:
+            TOKEN = file.read() 
+    if not (TOKEN is None):
+        tmp['token'] = TOKEN     
+        if not path_exists(info_path):   
+            write_yaml(info_path,tmp)
     else:
-        tmp = load_yaml(info_path)
-        TOKEN = tmp['token']
-    if  TOKEN is None: 
-        TOKEN = get_token()
-    tmp['token'] = TOKEN
-    write_yaml(info_path,tmp)
-    
-
-    
-
+        raise NameError("Token is missing or wasn't set")        
